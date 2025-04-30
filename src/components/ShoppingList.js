a9a4a051a4f43bb935cbf147ceee0ad7f47d2953
Luce <boama561@gmail.com>
2025-04-30 13:18:04 +0000

    Mis a jour de nouvelles fonctionnalités

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,9 +1,12 @@
 
-import { plantList } from "../datas/plantsList";
-import '../styles/shoppingList.css';
-import PlantItem from './PlantItem';
+import { useState } from 'react'
+import { plantList } from '../datas/plantsList'
+import PlantItem from './PlantItem'
+import Categories from './Categories'
+import '../styles/ShoppingList.css'
 
 function ShoppingList({ cart, updateCart }) {
+	const [activeCategory, setActiveCategory] = useState('')
 	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
@@ -27,24 +30,27 @@ function ShoppingList({ cart, updateCart }) {
 
 	return (
 		<div className='lmj-shopping-list'>
-			<ul>
-				{categories.map((cat) => (
-					<li key={cat}>{cat}</li>
-				))}
-			</ul>
+			<Categories
+				categories={categories}
+				setActiveCategory={setActiveCategory}
+				activeCategory={activeCategory}
+			/>
+
 			<ul className='lmj-plant-list'>
-				{plantList.map(({ id, cover, name, water, light, price }) => (
-					<div key={id}>
-						<PlantItem
-							cover={cover}
-							name={name}
-							water={water}
-							light={light}
-							price={price}
-						/>
-						<button onClick={() => addToCart(name, price)}>Ajouter</button>
-					</div>
-				))}
+				{plantList.map(({ id, cover, name, water, light, price, category }) =>
+					!activeCategory || activeCategory === category ? (
+						<div key={id}>
+							<PlantItem
+								cover={cover}
+								name={name}
+								water={water}
+								light={light}
+								price={price}
+							/>
+							<button onClick={() => addToCart(name, price)}>Ajouter</button>
+						</div>
+					) : null
+				)}
 			</ul>
 		</div>
 	)
